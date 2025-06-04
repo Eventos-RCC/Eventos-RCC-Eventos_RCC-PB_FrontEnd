d958b92b1a5b3c3d0949bd6301766ebcce57f459
aldoalbuquerquejr <aldocandeia000@gmail.com>
2025-06-04 20:46:11 -0300

    LandingPage changes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,11 +5,19 @@ export function LandingPage() {
         <>
             <HeaderBar />
             <div
-                className={`flex h-[600px] w-full items-center justify-center bg-[url('/assets/rcc-landing-img.png')] from-transparent to-white bg-cover bg-center bg-no-repeat`}
+                className={`flex relative h-[600px] w-full items-center justify-center bg-[url('/assets/rcc-landing-img.png')] from-transparent to-white bg-cover bg-center bg-no-repeat`}
             >
-                <h1 className="text-white text-4xl font-bold text-center">
-                    Encontre e participe de eventos da Renovação<br/>Carismática Católica da Paraíba
-                </h1>
+                <div className="flex flex-col absolute items-center justify-center">
+                    <img
+                        src="/assets/rccpb-04.png"
+                        alt="logo-register-rccpb"
+                        className="w-1/5 h-auto object-cover filter brightness-0 invert"
+                        draggable="false"
+                    />
+                    <h1 className="text-white text-4xl font-bold text-center">
+                        Encontre e participe de eventos da Renovação<br />Carismática Católica da Paraíba
+                    </h1>
+                </div>
             </div>
         </>
     )
